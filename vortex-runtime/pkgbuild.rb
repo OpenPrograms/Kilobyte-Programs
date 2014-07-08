8d359feb3903be8599f4b900e0714c5901fdeff9
Kilobyte22 <stiepen22@gmx.de>
2014-07-09 01:46:17 +0200

    Derp derp derp derp

--- vortex-runtime/pkgbuild.rb
+++ vortex-runtime/pkgbuild.rb
@@ -4,6 +4,7 @@ description 'Vortex - A functional language that compiles to lua'
 
 install Dir['lib/*.lua'] => '/lib'
 install Dir['lib/vortex/*.lua'] => '/lib/vortex'
+install Dir['lib/vortex/rt/*.lua'] => '/lib/vortex/rt'
 install Dir['lib/vxrt/*.lua'] => '/lib/vxrt'
 
 authors 'Kilobyte'
